8224861050d25468cc78f5c687ab94ccd37ac204
Anyi-Global <ikeifeanyi95.ii@gmail.com>
2025-05-05 01:04:10 -0700

    Added authentication

--- app/views.py
+++ app/views.py
@@ -20,12 +20,15 @@ from functools import wraps
 
 # app.config = os.urandom(24)
 
-# Nigerian time function
 def nigerian_time():
+    '''
+    This is a function that extracts
+    the current Nigerian time
+    '''
     now = datetime.datetime.utcnow() + datetime.timedelta(hours=1)
     today = datetime.date.today()
     d2 = today.strftime("%B %d, %Y")
-    tm = now.strftime("%H:%M:%S %p")
+    tm = now.strftime("%H:%M:%S:%p")
     return (d2 +' '+'at'+' '+tm)
 
 def login_required(f):
@@ -73,6 +76,7 @@ def recipe_search():
 def search():
     ingredients = request.form['ingredients']
     dietary_preferences = request.form.getlist('preferences')
+    cuisine_type = request.form.get('cuisineType')  # Get the selected cuisine type
 
     # Edamam API URL
     api_url = 'https://api.edamam.com/search'
@@ -80,10 +84,156 @@ def search():
         'q': ingredients,
         'app_id': os.getenv('EDAMAM_API_ID'),
         'app_key': os.getenv('EDAMAM_API_KEY'),
-        'health': dietary_preferences
+        'health': dietary_preferences,
+        'cuisineType': cuisine_type.lower()
     }
+
+    # # If the user selected any dietary preferences, add them
+    # if dietary_preferences:
+    #     params['health'] = dietary_preferences
+
+    # # If the user selected a cuisine type, add it
+    # if cuisine_type:
+    #     params['cuisineType'] = cuisine_type.lower()  # Edamam expects cuisine types in lowercase
     
     response = requests.get(api_url, params=params)
+
     data = response.json()
 
     return jsonify(data)
+
+# SignUp EndPoint
+@app.route("/sign-up", methods=["GET", "POST"])
+def sign_up():
+    if request.method=='POST':
+        req = request.form
+        
+        username = str(req["username"])
+        email = str(req["email"]).lower()
+        password = req["pswd"]
+        con_password = req["con_pswd"]
+        
+        if password != con_password:
+            flash("Password Confirmation Mismatched, Please Confirm Your Password!", "danger")
+            return render_template("public/register.html")
+        # checkuser = mongo.db.sign_up.find_one({"matric_number":mat_no}, {"_id":0})
+        checkuser = mongo.db.signup.find_one({username:{"$exists":True}}, {"_id":0})
+        if checkuser:
+            flash("Sorry, User already registered!", "danger")
+            return render_template("public/register.html")
+        # checkemail = mongo.db.sign_up.find_one({"email":email}, {"_id":0})
+        checkemail = mongo.db.signup.find_one({email:{"$exists":True}}, {"_id":0})
+        if checkemail:
+            flash("Sorry, User with email address already exists!", "danger")
+            return render_template("public/register.html")
+        
+        mongo.db.signup.insert_one({"username": username, username:username, "email": email, email:email, "password": password, "activationStatus":"0", "registeredDate": nigerian_time()})
+        flash("Account Created Successfully!", "success")
+        return redirect(url_for("index"))
+    else:
+        return render_template("public/register.html")
+
+# Login EndPoint
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method=='POST':
+        req = request.form
+        
+        username = str(req["username"])
+        pswd = req["pswd"]
+        
+        checkuser = mongo.db.signup.find_one({username:{"$exists":True}}, {"_id":0})
+        
+        # x = re.search(pattern, string)
+        
+        if not checkuser:
+            flash("Username/Password Incorrect!", "danger")
+            return render_template("public/index.html")
+        
+        # elif checkuser["activationStatus"] != "1":
+        #     flash("Account not activated. Contact Admin for account activation!", "danger")
+        #     return render_template("public/index.html")
+        
+        elif not pswd == checkuser["password"]:
+            flash("Username/Password Incorrect!", "danger")
+            return render_template("public/index.html")
+                  
+        else:
+            del checkuser["password"]
+            session["user"] = checkuser
+            session["login"]=True
+            if checkuser["username"] == "admin":
+                flash("Logged in Successfully! Welcome to your Dashboard!!", "success")
+                return redirect(url_for("admin_dashboard"))
+            
+            else:
+                flash("Logged in Successfully! Welcome to your Dashboard!!", "success")
+                return redirect(url_for("users_dashboard"))
+    
+    return render_template("public/index.html")
+
+# Edit Profile EndPoint (though not neccessary for this project)
+@app.route("/edit-profile", methods=["GET", "POST"])
+@login_required
+def edit_profile():
+    if request.method=='POST':
+        req = request.form
+        print(req)
+    
+        username = req["username"]
+        new_email = req["email"]
+    
+        # checkuser = mongo.db.sign_up.find_one({"matric_number":mat_no}, {"_id":0})
+        checkuser = mongo.db.signup.find_one({username:{"$exists":True}}, {"_id":0})
+        if not checkuser:
+            flash("Sorry, User not registered!", "danger")
+            return render_template("public/student_profile.html")
+        # checkemail = mongo.db.sign_up.find_one({"email":email}, {"_id":0})
+        checkemail = mongo.db.signup.find_one({new_email:{"$exists":True}}, {"_id":0})
+        if checkemail:
+            flash("Sorry, User with email address already exists!", "danger")
+            return render_template("public/student_profile.html")
+    
+        old_email = checkuser["email"]
+        # mongo.db.sign_up.update_one({mat_no:{"$exists":True}}, {"$unset": {"email": old_email,:new_email}})
+        mongo.db.signup.update_one({username:{"$exists":True}}, {"$set": {"email": new_email}, "$unset": {old_email: ""}})
+        flash("Profile Updated Successfully!", "success")
+        return redirect(url_for("student_profile"))
+    
+    return render_template("public/student_profile.html")
+
+# Change Password EndPoint (though not neccessary for this project)
+@app.route("/change-password", methods=["GET", "POST"])
+@login_required
+def change_password():
+    if request.method=='POST':
+        req = request.form
+    
+        currentpswd = req["currentpassword"]
+        newpswd = req["newpassword"]
+        renewpswd = req["renewpassword"]
+    
+        # checkuser = mongo.db.sign_up.find_one({"matric_number":mat_no}, {"_id":0})
+        checkpassword = mongo.db.signup.find_one({currentpswd:{"$exists":True}}, {"_id":0})
+        if not checkpassword:
+            flash("Sorry, Incorrect Password", "danger")
+            return render_template("public/student_profile.html")
+        # checkemail = mongo.db.sign_up.find_one({"email":email}, {"_id":0})
+        if newpswd == currentpswd:
+            flash("Sorry, Current password and New password must be different!", "danger")
+            return render_template("public/student_profile.html")
+        if newpswd != renewpswd:
+            flash("New password do not match!", "danger")
+            return render_template("public/student_profile.html")
+        # checkemail = mongo.db.signup.find_one({new_email:{"$exists":True}}, {"_id":0})
+        # if checkemail:
+        #     flash("Sorry, User with email address already exists!", "danger")
+        #     return render_template("public/student_profile.html")
+        
+        old_pswd = checkpassword['password']
+        # mongo.db.sign_up.update_one({mat_no:{"$exists":True}}, {"$unset": {"email": old_email,:new_email}})
+        mongo.db.signup.update_one({currentpswd:{"$exists":True}}, {"$set": {"password": newpswd}, "$unset": {old_pswd: ""}})
+        flash("Password Changed Successfully!", "success")
+        return redirect(url_for("student_profile"))
+    
+    return render_template("public/student_profile.html")
