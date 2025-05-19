826c08552ffd0362965de61a63558087464c2351
Anyi-global <ikeifeanyi95.ii@gmail.com>
2025-05-18 21:50:11 -0700

    Modified __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -15,19 +15,16 @@ app.config['EDAMAM_API_ID'] = os.getenv('EDAMAM_API_ID')
 app.config['EDAMAM_API_KEY'] = os.getenv('EDAMAM_API_KEY')
 app.config['MONGO_URI'] = os.getenv('MONGO_URI')
 
-#app configuration
-app_settings = os.environ.get(
-    'APP_SETTINGS'
-    'app.config'
-) 
-app.config.from_object(app_settings)
-
 #initializing PyMongo
-mongo = PyMongo(app, app.config['MONGO_URI'])
+mongo = PyMongo(app)
+
+'''
+This is a solution for MAC OS Mongodb certificate issue
+
 # ca = certifi.where()
 # client = MongoClient(app.config['MONGO_URI'], tlsCAFile=ca)
 # # mongo = client.get_default_database()
 # mongo = client["RecipeFinderApp"]
-
+'''
 
 from app import views
